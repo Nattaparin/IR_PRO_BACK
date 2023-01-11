2ff29e760f49c76fe0a86cb5da2730d1cda3f98e
Nattaparin <nattaparin455@gmail.com>
2023-01-12 00:48:35 +0700

    model

---
+++ model/__init__.py
@@ -0,0 +1,13 @@
+import bcrypt
+from sqlalchemy import event
+from .user import User
+from .database import db
+
+
+@event.listens_for(User.__table__, 'after_create')
+def create_user(*args, **kwargs):
+    db.session.add(
+        User(username='smart', password=bcrypt.hashpw('smart'.encode('utf-8'), bcrypt.gensalt(10)),
+             email='smart@hotmail.com',
+             bookmark=None, favorite=None))
+    db.session.commit()
